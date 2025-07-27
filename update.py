5063121cb61e963d353d97556a89314c8b24a86f
Pmodugula89 <modugulp@csp.edu>
2025-07-27 19:55:25 -0400

    Update update.py

--- update.py
+++ update.py
@@ -1,8 +1,2 @@
-import pandas as pd
-
 def save_inventory(df, output_path):
-    try:
-        df.to_csv(output_path, index=False)
-        print(f"✅ Cleaned inventory saved to {output_path}")
-    except Exception as e:
-        print(f"❌ Error saving file: {e}")
+    df.to_csv(output_path, index=False)
